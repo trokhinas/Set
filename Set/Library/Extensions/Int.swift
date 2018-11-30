557bd0b390c6a6b1a2e1c58e4a4b680d3a20b2b8
Anton <anton@mail.com>
2018-11-30 15:53:55 +0300

    version 0.1

---
+++ Set/Library/Extensions/Int.swift
@@ -0,0 +1,9 @@
+//
+//  Int.swift
+//  Set
+//
+//  Created by xcode on 30.11.2018.
+//  Copyright © 2018 VSU. All rights reserved.
+//
+
+import Foundation
---
+++ Set/Models/SetCardDeck.swift
@@ -0,0 +1,13 @@
+//
+//  SetDeckCards.swift
+//  Set
+//
+//  Created by xcode on 30.11.2018.
+//  Copyright © 2018 VSU. All rights reserved.
+//
+
+import Foundation
+
+struct SetDeckCards {
+    <#fields#>
+}
